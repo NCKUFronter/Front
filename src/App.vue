d9abf67a202d898a7b3326f1c5f0a331db196228
p0o9i8u7y6t5r4e3w2q1 <rose8783@gmail.com>
2020-04-15 21:13:04 +0800

    coonect backend

--- src/App.vue
+++ src/App.vue
@@ -1,111 +1,107 @@
 <template>
-    <div id="app">
-        <div class="header">
-            <a href="##home###" class="logo">記帳網</a>
-        </div>
-        <div class="container">
-            <div class="side-menu">
-                <div class="menu-item" v-for="(item,index) in menu" :key="index">
-                    <router-link :to="item.link" class="title">{{item.title}}</router-link>
-                    <!-- router-link只是表示換哪個component網址 -->
-                </div>
-            </div>
-            <!-- 使用sideAccount這個component,並傳入變數accountData -->
-            <!--SideAccount :accountData="accountData1" /-->
-            <!-- :引入SideAccount變數名稱="在App的變數名稱" -->
-            
-            <router-view></router-view>
-            <!-- 真正幫你引入component -->
-        </div>
+  <div id="app">
+    <div class="header">
+      <a href="##home###" class="logo">記帳網</a>
     </div>
+    <div class="container">
+      <div class="side-menu">
+        <div class="menu-item" v-for="(item,index) in menu" :key="index">
+          <router-link :to="item.link" class="title">{{item.title}}</router-link>
+          <!-- router-link只是表示換哪個component網址 -->
+        </div>
+      </div>
+      <!-- 使用sideAccount這個component,並傳入變數accountData -->
+      <!--SideAccount :accountData="accountData1" /-->
+      <!-- :引入SideAccount變數名稱="在App的變數名稱" -->
+
+      <router-view></router-view>
+      <!-- 真正幫你引入component -->
+    </div>
+  </div>
 </template>
 
 <script>
 // 定義component,不是global,只有APP知道
 //import SideAccount from './components/SideAccount.vue'
 
-let data={
-
-    //  accountData1:[
-    //     {src:'#',category:'收支項名',money:'金額',account:'帳戶'},
-    //     {src:'#',category:'收支項名',money:'金額',account:'帳戶'},
-    //     {src:'#',category:'收支項名',money:'金額',account:'帳戶'}
-    // ],
-    menu:[
-        {title:'統計圖產生',link:'/HelloWorld'},
-        {title:'周/月帳目一覽',link:'/'},
-        {title:'類別編輯',link:''},
-        {title:'帳戶管理',link:''},
-        {title:'雲端備分',link:''},
-        {title:'統一發票',link:''}
-    ]
-}
+let data = {
+  //  accountData1:[
+  //     {src:'#',category:'收支項名',money:'金額',account:'帳戶'},
+  //     {src:'#',category:'收支項名',money:'金額',account:'帳戶'},
+  //     {src:'#',category:'收支項名',money:'金額',account:'帳戶'}
+  // ],
+  menu: [
+    { title: "統計圖產生", link: "/HelloWorld" },
+    { title: "周/月帳目一覽", link: "/" },
+    { title: "類別編輯", link: "" },
+    { title: "帳戶管理", link: "" },
+    { title: "雲端備分", link: "" },
+    { title: "統一發票", link: "" }
+  ]
+};
 
 export default {
-  name: 'App',
+  name: "App",
   data() {
     return data;
   },
   components: {
-    //SideAccount  // 定義component  
+    //SideAccount  // 定義component
   },
-  computed:{
-       
-  },
-  methods:{
-    
-  }
-}
-
+  computed: {},
+  methods: {}
+};
 </script>
 
 <style>
-  body{
-      margin: 0;
-      padding: 0;
-      font-family: 微軟正黑體,Arial, Helvetica, sans-serif;
-  }
+body {
+  margin: 0;
+  padding: 0;
+  font-family: 微軟正黑體, Arial, Helvetica, sans-serif;
+}
 
-  .header{
-      background-color: #efca16;
-      height: 6vh;
-      position: relative;
-  }
+.header {
+  background-color: #efca16;
+  height: 6vh;
+  position: relative;
+}
 
-  .logo{
-      display: inline-block;
-      text-decoration: none;
-      color: black;
-      height: 6vh;
-      margin: auto;
-      font-size: 4vh;
-      margin-left: 10px;
-  }
+.logo {
+  display: inline-block;
+  text-decoration: none;
+  color: black;
+  height: 6vh;
+  margin: auto;
+  font-size: 4vh;
+  margin-left: 10px;
+}
 
-  .container{
-      display: flex;
-  }
+.container {
+  display: flex;
+}
 
-  .side-menu{
-      border-right: 3px #CCCCCC solid;
-      width: 18vw;
-      height: 94vh;
-  }
+.side-menu {
+  border-right: 3px #cccccc solid;
+  width: 18vw;
+  height: 94vh;
+}
 
-  .menu-item{
-      border: solid #d9d1ba;
-      border-radius: 10px;
-      margin: 5%;
-      height: fit-content;
-      width: auto;
-      padding: 20px;
-  }
-  .side-menu a{
-      font-weight: normal;
-      text-decoration: none;
-      color: black;
-  }
+.menu-item {
+  border: solid #d9d1ba;
+  border-radius: 10px;
+  margin: 5%;
+  height: fit-content;
+  width: auto;
+  padding: 20px;
+}
+.side-menu a {
+  font-weight: normal;
+  text-decoration: none;
+  color: black;
+}
 
-  
-  
+/* other global classes */
+.cursor-pointer {
+  cursor: pointer;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,20 +1,19 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import axios from 'axios'
-import VueAxios from 'vue-axios'
- 
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import axios from "axios";
+import VueAxios from "vue-axios";
 
 //把component定義為global
 // import SideAccount from './components/SideAccount.vue'
 // Vue.component('account',SideAccount)
 // ('component名稱',component object)
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-Vue.use(VueAxios, axios) // $http
+Vue.use(VueAxios, axios.create({ baseURL: "http://localhost:3000" })); // $http
 
 new Vue({
   router,
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
